7d50770f60c6e8256b67ca87a31e28425f24c34c
Pedro Duque Vieira <pedro.duquevieira@gmail.com>
2013-10-29 00:24:40 +0000

    Changes to the checkbox both to the light and dark theme

--- src/jmetro/Main.java
+++ src/jmetro/Main.java
@@ -38,12 +38,12 @@ import org.scenicview.ScenicView;
 public class Main extends Application {
     static final String CHECK_BOX_RESOURCE = "JMetro CheckBox.fxml";
     static final String CONTEXT_MENU_RESOURCE = "JMetro ContextMenu.fxml";
-    static final String RESOURCE = CONTEXT_MENU_RESOURCE;
+    static final String RESOURCE = CHECK_BOX_RESOURCE;
 
     static final String LIGHT_STYLE_SHEET = "JMetroLightTheme.css";
     static final String DARK_STYLE_SHEET = "JMetroDarkTheme.css";
 
-    static final String STYLE_SHEET = DARK_STYLE_SHEET;
+    static final String STYLE_SHEET = LIGHT_STYLE_SHEET;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
