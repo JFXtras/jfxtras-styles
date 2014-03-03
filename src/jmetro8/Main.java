f3dea06a1fe8755ccf5262d28a157c2252bbabad
Pedro Duque Vieira <pedro.duquevieira@gmail.com>
2014-03-03 14:52:29 +0000

    clean-up combobox code and fix some issues

--- src/jmetro8/Main.java
+++ src/jmetro8/Main.java
@@ -52,12 +52,12 @@ public class Main extends Application {
     static final String PASSWORDBOX_RESOURCE = "JMetro PasswordBox.fxml";
     static final String PROGRESSBAR_RESOURCE = "JMetro ProgressBar.fxml";
 
-    static final String RESOURCE = PROGRESSBAR_RESOURCE;
+    static final String RESOURCE = COMBOBOX_RESOURCE;
 
     static final String LIGHT_STYLE_SHEET = "JMetroLightTheme.css";
     static final String DARK_STYLE_SHEET = "JMetroDarkTheme.css";
 
-    static final String STYLE_SHEET = LIGHT_STYLE_SHEET;
+    static final String STYLE_SHEET = DARK_STYLE_SHEET;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
