042afdda065d5b38bf3542591f604513b0d2eab8
Pedro Duque Vieira <pedro.duquevieira@pixelduke.com>
2018-02-19 00:50:31 +0000

    Revert "JMetro: Change presentation test code"
    This reverts commit 2efb9fee41b2bde7a946b211a7484614ceecd91a.

--- src/jmetro8/SpinnerController.java
+++ src/jmetro8/SpinnerController.java
@@ -2,7 +2,6 @@ package jfxtras.styles.jmetro8;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Pos;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
 
@@ -14,25 +13,15 @@ import java.util.ResourceBundle;
  */
 public class SpinnerController implements Initializable
 {
-    @FXML private Spinner spinner1;
-    @FXML private Spinner spinner2;
-    @FXML private Spinner spinner3;
-    @FXML private Spinner spinner4;
-    @FXML private Spinner spinner5;
-    @FXML private Spinner spinner6;
+    @FXML private Spinner spinner;
     @FXML private Spinner disabledSpinner;
 
     @Override
     public void initialize(URL location, ResourceBundle resources)
     {
-        spinner1.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 10));
-        spinner2.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 10));
-        spinner3.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 10));
-        spinner4.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 10));
-        spinner5.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 10));
-        spinner6.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 10));
+//        spinner.getStyleClass().add(Spinner.STYLE_CLASS_SPLIT_ARROWS_HORIZONTAL);
+//        disabledSpinner.getStyleClass().add(Spinner.STYLE_CLASS_SPLIT_ARROWS_HORIZONTAL);
 
-        spinner6.getEditor().setAlignment(Pos.CENTER);
-        disabledSpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 10));
+        spinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 10));
     }
 }
