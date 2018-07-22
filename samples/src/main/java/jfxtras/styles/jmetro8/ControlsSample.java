367843aefdd001ed866e22214482de8280f7e2be
Pedro Duque Vieira <pedro.duquevieira@pixelduke.com>
2018-07-22 15:41:51 +0100

    Update Check Box light style for JMetro

--- samples/src/main/java/jfxtras/styles/jmetro8/ControlsSample.java
+++ samples/src/main/java/jfxtras/styles/jmetro8/ControlsSample.java
@@ -55,7 +55,7 @@ public class ControlsSample extends Application {
     static final private String CHOICE_BOX_RESOURCE = "JMetro ChoiceBox.fxml";
     static final private String LIST_BOX_RESOURCE = "JMetro ListBox.fxml";
 
-    static final private String RESOURCE = RADIO_BUTTON_RESOURCE;
+    static final private String RESOURCE = CHECK_BOX_RESOURCE;
 
     static final private JMetro.Style STYLE = JMetro.Style.LIGHT;
 
