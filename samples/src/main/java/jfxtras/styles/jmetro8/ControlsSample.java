5911e92d2da50b9cae87ec2107b8fa8aae7423f0
Pedro Duque Vieira <pedro.duquevieira@pixelduke.com>
2018-07-25 00:55:05 +0100

    JMetro: Add Choice Box dark style

--- samples/src/main/java/jfxtras/styles/jmetro8/ControlsSample.java
+++ samples/src/main/java/jfxtras/styles/jmetro8/ControlsSample.java
@@ -55,7 +55,7 @@ public class ControlsSample extends Application {
     static final private String CHOICE_BOX_RESOURCE = "JMetro ChoiceBox.fxml";
     static final private String LIST_BOX_RESOURCE = "JMetro ListBox.fxml";
 
-    static final private String RESOURCE = CONTEXT_MENU_RESOURCE;
+    static final private String RESOURCE = CHOICE_BOX_RESOURCE;
 
     static final private JMetro.Style STYLE = JMetro.Style.DARK;
 
