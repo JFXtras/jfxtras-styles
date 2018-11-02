f443bdfadb765dbb70696840f120e589cd481f92
Pedro Duque Vieira <pedro.duquevieira@pixelduke.com>
2018-11-02 19:43:43 +0000

    JMetro: Change used rating control colors based on ACCENT_COLOR CSS var

--- samples/src/main/java/jfxtras/styles/jmetro8/ControlsSample.java
+++ samples/src/main/java/jfxtras/styles/jmetro8/ControlsSample.java
@@ -55,9 +55,9 @@ public class ControlsSample extends Application {
     static final private String CHOICE_BOX_RESOURCE = "JMetro ChoiceBox.fxml";
     static final private String LIST_VIEW_RESOURCE = "JMetro ListView.fxml";
 
-    static final private String RESOURCE = PROGRESS_BAR_RESOURCE;
+    static final private String RESOURCE = RATING_RESOURCE;
 
-    static final private JMetro.Style STYLE = JMetro.Style.DARK;
+    static final private JMetro.Style STYLE = JMetro.Style.LIGHT;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
