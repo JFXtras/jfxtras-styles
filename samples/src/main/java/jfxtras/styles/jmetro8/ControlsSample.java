b11b99c8b6eaef97b8ec3dcb87b1a3df006bb525
Pedro Duque Vieira <pedro.duquevieira@pixelduke.com>
2018-11-17 19:17:13 +0000

    JMetro: preliminary refactor and issue fixing of spinner CSS

--- samples/src/main/java/jfxtras/styles/jmetro8/ControlsSample.java
+++ samples/src/main/java/jfxtras/styles/jmetro8/ControlsSample.java
@@ -59,7 +59,7 @@ public class ControlsSample extends Application {
     static final private String CHOICE_BOX_RESOURCE = "JMetro ChoiceBox.fxml";
     static final private String LIST_VIEW_RESOURCE = "JMetro ListView.fxml";
 
-    static final private String RESOURCE = TEXT_AREA_RESOURCE;
+    static final private String RESOURCE = SPINNER_RESOURCE;
     static final private JMetro.Style STYLE = JMetro.Style.LIGHT;
 
     @Override
