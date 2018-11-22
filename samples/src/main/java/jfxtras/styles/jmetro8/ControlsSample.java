720d0cf29e188cfc9ff2791310b58a4fff2a706e
Pedro Duque Vieira <pedro.duquevieira@pixelduke.com>
2018-11-22 00:52:45 +0000

    JMetro: Refactor JMetro CSS - Remove duplicate code in List View

--- samples/src/main/java/jfxtras/styles/jmetro8/ControlsSample.java
+++ samples/src/main/java/jfxtras/styles/jmetro8/ControlsSample.java
@@ -59,8 +59,8 @@ public class ControlsSample extends Application {
     static final private String CHOICE_BOX_RESOURCE = "JMetro ChoiceBox.fxml";
     static final private String LIST_VIEW_RESOURCE = "JMetro ListView.fxml";
 
-    static final private String RESOURCE = CHOICE_BOX_RESOURCE;
-    static final private JMetro.Style STYLE = JMetro.Style.LIGHT;
+    static final private String RESOURCE = LIST_VIEW_RESOURCE;
+    static final private JMetro.Style STYLE = JMetro.Style.DARK;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
