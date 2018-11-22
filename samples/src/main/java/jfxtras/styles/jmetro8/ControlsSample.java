77e69e85d5480cc5614d870e2b4370129bc3477a
Pedro Duque Vieira <pedro.duquevieira@pixelduke.com>
2018-11-22 00:23:53 +0000

    JMetro: Refactor JMetro CSS - Remove duplicate code in Choice Box

--- samples/src/main/java/jfxtras/styles/jmetro8/ControlsSample.java
+++ samples/src/main/java/jfxtras/styles/jmetro8/ControlsSample.java
@@ -59,7 +59,7 @@ public class ControlsSample extends Application {
     static final private String CHOICE_BOX_RESOURCE = "JMetro ChoiceBox.fxml";
     static final private String LIST_VIEW_RESOURCE = "JMetro ListView.fxml";
 
-    static final private String RESOURCE = DATE_PICKER_RESOURCE;
+    static final private String RESOURCE = CHOICE_BOX_RESOURCE;
     static final private JMetro.Style STYLE = JMetro.Style.LIGHT;
 
     @Override
