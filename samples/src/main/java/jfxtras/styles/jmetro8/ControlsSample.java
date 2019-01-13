0019345458398f0cb3252ae57d49b53e7f367acd
Pedro Duque Vieira <pedro.duquevieira@gmail.com>
2019-01-13 17:19:19 +0000

    Fixes #30: New Accordion light and dark style and remade Titled Pane styles

--- samples/src/main/java/jfxtras/styles/jmetro8/ControlsSample.java
+++ samples/src/main/java/jfxtras/styles/jmetro8/ControlsSample.java
@@ -59,8 +59,9 @@ public class ControlsSample extends Application {
     static final private String CHOICE_BOX_RESOURCE = "JMetro ChoiceBox.fxml";
     static final private String LIST_VIEW_RESOURCE = "JMetro ListView.fxml";
     static final private String TITLED_PANE_RESOURCE = "JMetro TitledPane.fxml";
+    static final private String ACCORDION_RESOURCE = "JMetro Accordion.fxml";
 
-    static final private String RESOURCE = TITLED_PANE_RESOURCE;
+    static final private String RESOURCE = ACCORDION_RESOURCE;
     static final private JMetro.Style STYLE = JMetro.Style.LIGHT;
 
     @Override
