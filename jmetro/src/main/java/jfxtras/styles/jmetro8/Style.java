0e07cfa6fde15891cfe4e626c8fae52547543b1f
Pavel Erokhin <MairwunNx@gmail.com>
2019-03-12 19:39:46 +0300

    Just shortening code lines for Style enum.

--- jmetro/src/main/java/jfxtras/styles/jmetro8/Style.java
+++ jmetro/src/main/java/jfxtras/styles/jmetro8/Style.java
@@ -7,14 +7,8 @@ public enum Style {
     protected String getStyleSheetFileName() {
         String stylesheet = null;
 
-        switch (this) {
-            case LIGHT:
-                stylesheet = "JMetroLightTheme.css";
-                break;
-            case DARK:
-                stylesheet = "JMetroDarkTheme.css";
-                break;
-        }
+        if (this == Style.LIGHT) stylesheet = "JMetroLightTheme.css";
+        else if (this == Style.DARK) stylesheet = "JMetroDarkTheme.css";
 
         return stylesheet;
     }
