de2e1e15332610e0fdb5663ec1b24dd9adcd432d
Pedro Duque Vieira <pedro.duquevieira@gmail.com>
2019-05-07 22:46:22 +0100

    JMetro: implement size fixes for controls (#63 and #64) for Menus, Context Menus, Menu Bar and Menu Item

--- samples/src/main/java/jfxtras/styles/jmetro/ControlsSample.java
+++ samples/src/main/java/jfxtras/styles/jmetro/ControlsSample.java
@@ -63,7 +63,7 @@ public class ControlsSample extends Application {
     private static final String MENU_BUTTON_RESOURCE = "JMetro MenuButton.fxml";
     private static final String HYPERLINK_RESOURCE = "JMetro Hyperlink.fxml";
 
-    static final private String RESOURCE = BUTTON_RESOURCE;
+    static final private String RESOURCE = CONTEXT_MENU_RESOURCE;
     static final private JMetro.Style STYLE = JMetro.Style.LIGHT;
 
     @Override
