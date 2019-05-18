e42c2470fbeae32ee6ed81a3f23ccc3a7a04fd93
Pedro Duque Vieira <pedro.duquevieira@gmail.com>
2019-05-18 19:18:46 +0100

    JMetro: Fix background colors for MenuButtons, Menus, ContextMenus and Menu Items (issue #39)

--- samples/src/main/java/jfxtras/styles/jmetro/ControlsSample.java
+++ samples/src/main/java/jfxtras/styles/jmetro/ControlsSample.java
@@ -63,8 +63,8 @@ public class ControlsSample extends Application {
     private static final String MENU_BUTTON_RESOURCE = "JMetro MenuButton.fxml";
     private static final String HYPERLINK_RESOURCE = "JMetro Hyperlink.fxml";
 
-    static final private String RESOURCE = CONTEXT_MENU_RESOURCE;
-    static final private JMetro.Style STYLE = JMetro.Style.LIGHT;
+    static final private String RESOURCE = MENU_BUTTON_RESOURCE;
+    static final private JMetro.Style STYLE = JMetro.Style.DARK;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
