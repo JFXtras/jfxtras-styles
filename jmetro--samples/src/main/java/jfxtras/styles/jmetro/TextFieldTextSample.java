8598fa895c22d670ac94c378dae4099ebb96fd9c
Pedro Duque Vieira <pedro.duquevieira@gmail.com>
2019-11-01 22:45:07 +0000

    Fixes #126, fixes #125: Text clear (x) button shows up when it shouldn't on some cases

---
+++ jmetro--samples/src/main/java/jfxtras/styles/jmetro/TextFieldTextSample.java
@@ -0,0 +1,50 @@
+package jfxtras.styles.jmetro;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class TextFieldTextSample extends Application {
+
+    private static final Style STYLE = Style.LIGHT;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        System.setProperty("prism.lcdtext", "false");
+
+        VBox vbox = new VBox();
+        Scene scene = new Scene(vbox);
+        stage.setTitle("Table View Sample");
+        stage.setWidth(650);
+        stage.setHeight(450);
+
+        TextField textField = new TextField();
+
+        Button textFieldSetText = new Button("Set text");
+        textFieldSetText.setOnAction(event -> textField.setText("Some text"));
+        Button textFieldDisable = new Button("Disable");
+        textFieldDisable.setOnAction(event -> textField.setDisable(!textField.isDisabled()));
+
+        vbox.setSpacing(40);
+        vbox.setPadding(new Insets(10, 10, 10, 10));
+
+        VBox controlsVBox = new VBox();
+        controlsVBox.getChildren().addAll(textFieldSetText, textFieldDisable);
+        controlsVBox.setSpacing(10);
+
+        vbox.getChildren().addAll(textField, controlsVBox);
+
+        new JMetro(scene, STYLE);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+}
+
