7c2d3a3b91da6a0ab356f1dcfd136c8518bdd061
Pedro Duque Vieira <pedro.duquevieira@gmail.com>
2019-12-17 22:33:00 +0000

    Add new TabPane style with underline

---
+++ jmetro--samples/src/main/java/jfxtras/styles/jmetro/TabPaneWithUnderlineStyleSample.java
@@ -0,0 +1,63 @@
+package jfxtras.styles.jmetro;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class TabPaneWithUnderlineStyleSample extends Application {
+
+    private static final Style STYLE = Style.LIGHT;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public TabPaneWithUnderlineStyleSample() {
+    }
+
+    @Override
+    public void start(Stage stage) {
+        TabPane tabPane = new TabPane();
+        tabPane.getStyleClass().add("underlined");
+
+        Tab fileTab = new Tab();
+        fileTab.setClosable(false);
+        fileTab.setText("File");
+
+        Tab homeTab = new Tab();
+        homeTab.setClosable(false);
+        homeTab.setText("Home");
+
+        Tab insertTab = new Tab();
+        insertTab.setClosable(false);
+        insertTab.setText("Insert");
+
+        Tab tableTab = new Tab();
+        tableTab.setClosable(false);
+        tableTab.setText("Table");
+
+        Tab optionsTab = new Tab();
+        optionsTab.setClosable(false);
+        optionsTab.setText("Options");
+
+        tabPane.getTabs().addAll(fileTab, homeTab, insertTab, tableTab, optionsTab);
+
+        BorderPane root = new BorderPane(tabPane);
+
+        root.getStyleClass().add("background");
+
+        Scene scene = new Scene(root, 500, 200);
+
+        new JMetro(scene, STYLE);
+
+//        ScenicView.show(scene);
+
+
+        stage.setTitle("TabPane Sample");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
