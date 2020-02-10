15ecf11785b4b18d8e6fbf98fb4e5109f9179abf
Pedro Duque Vieira <pedro.duquevieira@pixelduke.com>
2020-02-10 19:38:53 +0000

    SplitPane - Fix wrong background color around SplitPane's borders
    (cherry picked from commit b23e680d19e290e772f42ac105c6827fc3b424e3)

---
+++ jmetro-samples/src/main/java/jfxtras/styles/samples/jmetro/SplitPaneSimpleSample.java
@@ -0,0 +1,52 @@
+package jfxtras.styles.jmetro;
+
+import javafx.application.Application;
+import javafx.geometry.Orientation;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class SplitPaneSimpleSample extends Application {
+
+    private static final Style STYLE = Style.DARK;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public SplitPaneSimpleSample() {
+    }
+
+    @Override
+    public void start(Stage stage) {
+        System.setProperty("prism.lcdtext", "false");
+
+        SplitPane mainSplitPane = new SplitPane();
+        mainSplitPane.setOrientation(Orientation.VERTICAL);
+        mainSplitPane.setDividerPosition(0, 0.7);
+
+        SplitPane horizontalSplitPane = new SplitPane();
+        horizontalSplitPane.setDividerPosition(0, 0.3);
+
+        ScrollPane scrollPane = new ScrollPane();
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.getStyleClass().add(JMetroStyleClass.BACKGROUND);
+        BorderPane borderPane2 = new BorderPane();
+        borderPane2.getStyleClass().add(JMetroStyleClass.BACKGROUND);
+
+        horizontalSplitPane.getItems().addAll(borderPane, scrollPane);
+        mainSplitPane.getItems().addAll(horizontalSplitPane, borderPane2);
+
+        BorderPane root = new BorderPane(mainSplitPane);
+        root.getStyleClass().add(JMetroStyleClass.BACKGROUND);
+        Scene scene = new Scene(root, 800, 600);
+
+        new JMetro(scene, STYLE);
+
+        stage.setTitle("SplitPane Simple Sample");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
