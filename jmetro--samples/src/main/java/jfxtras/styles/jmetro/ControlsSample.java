8d3d01ee2c22a9fb0874e4478482057052819afa
Pedro Duque Vieira <pedro.duquevieira@pixelduke.com>
2020-06-18 14:31:34 +0100

    Fixes #164: SplitMenuButton - Tweak hover effect when SplitMenuButton is inside a Toolbar

--- jmetro--samples/src/main/java/jfxtras/styles/jmetro/ControlsSample.java
+++ jmetro--samples/src/main/java/jfxtras/styles/jmetro/ControlsSample.java
@@ -70,7 +70,7 @@ public class ControlsSample extends Application {
 
     private static final String COLOR_PICKER_RESOURCE = "JMetro ColorPicker.fxml";
 
-    static final private String RESOURCE = LIST_VIEW_RESOURCE;
+    static final private String RESOURCE = SPLIT_MEU_BUTTON_RESOURCE;
     static final private Style STARTING_STYLE = Style.LIGHT;
 
     @Override
