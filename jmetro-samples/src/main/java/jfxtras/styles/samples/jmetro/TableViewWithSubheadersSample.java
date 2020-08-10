619e020f85a04fbf4f2e1520bfbcac01263cc6f1
Pedro Duque Vieira <pedro.duquevieira@pixelduke.com>
2020-08-10 23:31:05 +0100

    Issue #170: TreeTableView - Sort arrow isn't showing on secondary column
    (cherry picked from commit a7e42a57aaf40798cee3722a633035b31a570123)

---
+++ jmetro-samples/src/main/java/jfxtras/styles/samples/jmetro/TableViewWithSubheadersSample.java
@@ -0,0 +1,170 @@
+package jfxtras.styles.jmetro;
+
+import javafx.application.Application;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import static jfxtras.styles.jmetro.JMetroStyleClass.*;
+import static jfxtras.styles.jmetro.JMetroStyleClass.BACKGROUND;
+
+public class TableViewWithSubheadersSample extends Application {
+
+    private static final Style STYLE = Style.LIGHT;
+
+    private TableView<TableViewWithSubheadersSample.Person> table = new TableView<>();
+    private final ObservableList<TableViewWithSubheadersSample.Person> data =
+            FXCollections.observableArrayList(
+                    new TableViewWithSubheadersSample.Person("Jacob", "Smith", "jacob.smith@example.com"),
+                    new TableViewWithSubheadersSample.Person("Isabella", "Johnson", "isabella.johnson@example.com"),
+                    new TableViewWithSubheadersSample.Person("Ethan", "Williams", "ethan.williams@example.com"),
+                    new TableViewWithSubheadersSample.Person("Emma", "Jones", "emma.jones@example.com"),
+                    new TableViewWithSubheadersSample.Person("Michael", "Brown", "michael.brown@example.com")
+            );
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Style startingStyle = Style.LIGHT;
+        JMetro jMetro = new JMetro(startingStyle);
+
+        System.setProperty("prism.lcdtext", "false");
+
+        VBox vbox = new VBox();
+        Scene scene = new Scene(vbox);
+        stage.setTitle("Table View  With Subheaders Sample");
+        stage.setWidth(650);
+        stage.setHeight(450);
+
+        final Label header = new Label("Table View");
+        header.setPadding(new Insets(0, 0, 0, 5));
+        header.getStyleClass().add("header");
+
+        table.setEditable(true);
+        table.getSelectionModel().setCellSelectionEnabled(true);
+        table.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+
+        TableColumn firstNameCol = new TableColumn("First Name");
+        firstNameCol.setMinWidth(150);
+        firstNameCol.setCellValueFactory(new PropertyValueFactory<TableViewSample.Person, String>("firstName"));
+
+        TableColumn lastNameCol = new TableColumn("Last Name");
+        lastNameCol.setMinWidth(150);
+        lastNameCol.setCellValueFactory(new PropertyValueFactory<TableViewSample.Person, String>("lastName"));
+
+        TableColumn emailCol = new TableColumn("Email");
+        emailCol.setMinWidth(250);
+        emailCol.setCellValueFactory(new PropertyValueFactory<TableViewSample.Person, String>("email"));
+
+        TableColumn fullNameCol = new TableColumn("Full Name");
+        fullNameCol.getColumns().addAll(firstNameCol, lastNameCol);
+
+        table.setItems(data);
+        table.getColumns().addAll(fullNameCol, emailCol);
+
+        table.getSelectionModel().setCellSelectionEnabled(false);
+        CheckBox cellSelection = new CheckBox("Cell Selection");
+        cellSelection.setOnAction(event -> {
+            table.getSelectionModel().setCellSelectionEnabled(cellSelection.isSelected());
+        });
+        cellSelection.setSelected(table.getSelectionModel().isCellSelectionEnabled());
+
+        CheckBox tableButtonCheckBox = new CheckBox("Table Menu Button");
+        tableButtonCheckBox.setOnAction(event -> {
+            table.setTableMenuButtonVisible(tableButtonCheckBox.isSelected());
+        });
+        tableButtonCheckBox.setSelected(table.isTableMenuButtonVisible());
+
+        CheckBox alternatingRowColors = new CheckBox("Alternating Row Colors");
+        alternatingRowColors.setOnAction(event -> {
+            boolean isSelected = alternatingRowColors.isSelected();
+            if (isSelected) {
+                addIfNotPresent(table.getStyleClass(), ALTERNATING_ROW_COLORS);
+            } else {
+                table.getStyleClass().remove(ALTERNATING_ROW_COLORS);
+            }
+        });
+
+        CheckBox columnGridLines = new CheckBox("Grid Lines");
+        columnGridLines.setOnAction(event -> {
+            boolean isSelected = columnGridLines.isSelected();
+            if (isSelected) {
+                addIfNotPresent(table.getStyleClass(), TABLE_GRID_LINES);
+            } else {
+                table.getStyleClass().remove(TABLE_GRID_LINES);
+            }
+        });
+
+        vbox.setSpacing(40);
+        vbox.setPadding(new Insets(10, 10, 10, 10));
+
+        addIfNotPresent(vbox.getStyleClass(), BACKGROUND);
+
+        BorderPane controlsBorderPane = new BorderPane();
+        VBox controlsVBox = new VBox();
+        controlsVBox.getChildren().addAll(cellSelection, tableButtonCheckBox, alternatingRowColors, columnGridLines);
+        controlsVBox.setSpacing(10);
+
+        ComboBox<Style> jmetroStyleComboBox = new ComboBox<>();
+        jmetroStyleComboBox.getItems().addAll(Style.DARK, Style.LIGHT);
+        jmetroStyleComboBox.setValue(startingStyle);
+        jmetroStyleComboBox.valueProperty().addListener(observable -> jMetro.setStyle(jmetroStyleComboBox.getValue()));
+
+        controlsBorderPane.setLeft(controlsVBox);
+        controlsBorderPane.setRight(jmetroStyleComboBox);
+
+        vbox.getChildren().addAll(header, table, controlsBorderPane);
+
+        jMetro.setScene(scene);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static class Person {
+
+        private final SimpleStringProperty firstName;
+        private final SimpleStringProperty lastName;
+        private final SimpleStringProperty email;
+
+        public Person(String fName, String lName, String email) {
+            this.firstName = new SimpleStringProperty(fName);
+            this.lastName = new SimpleStringProperty(lName);
+            this.email = new SimpleStringProperty(email);
+        }
+
+        public String getFirstName() {
+            return firstName.get();
+        }
+
+        public void setFirstName(String fName) {
+            firstName.set(fName);
+        }
+
+        public String getLastName() {
+            return lastName.get();
+        }
+
+        public void setLastName(String fName) {
+            lastName.set(fName);
+        }
+
+        public String getEmail() {
+            return email.get();
+        }
+
+        public void setEmail(String fName) {
+            email.set(fName);
+        }
+    }
+}
---
+++ jmetro-samples/src/main/java/jfxtras/styles/samples/jmetro/TreeTableViewWithSubheaders.java
@@ -0,0 +1,232 @@
+package jfxtras.styles.jmetro;
+
+import javafx.application.Application;
+import javafx.beans.property.*;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static jfxtras.styles.jmetro.JMetroStyleClass.TABLE_GRID_LINES;
+import static jfxtras.styles.jmetro.JMetroStyleClass.addIfNotPresent;
+
+public class TreeTableViewWithSubheaders extends Application {
+
+    private static final Style STYLE = Style.DARK;
+
+    List<TreeTableViewWithSubheaders.Employee> employees = Arrays.asList(
+            new TreeTableViewWithSubheaders.Employee("Ethan Williams", "ethan.williams@example.com", "25", "Manager", "San Francisco", "3000"),
+            new TreeTableViewWithSubheaders.Employee("Emma Jones", "emma.jones@example.com", "30", "Sales Rep", "San Francisco", "3000"),
+            new TreeTableViewWithSubheaders.Employee("Michael Brown", "michael.brown@example.com", "50", "Consultant", "New York", "2000"),
+            new TreeTableViewWithSubheaders.Employee("Anna Black", "anna.black@example.com", "45", "Consultant", "Miami", "1000"),
+            new TreeTableViewWithSubheaders.Employee("Rodger York", "roger.york@example.com", "56", "Manager", "Washington", "5000"),
+            new TreeTableViewWithSubheaders.Employee("Susan Collins", "susan.collins@example.com", "32", "Director", "Florida", "6000"));
+
+    List<TreeTableViewWithSubheaders.Employee> itEmployees = Arrays.asList(
+            new TreeTableViewWithSubheaders.Employee("Jonathan James", "ethan.williams@example.com", "25", "Manager", "California", "7000"),
+            new TreeTableViewWithSubheaders.Employee("Pedro Duque Vieira", "emma.jones@example.com", "30", "Sales Rep", "California", "7000"),
+            new TreeTableViewWithSubheaders.Employee("Pavel Erokhin", "michael.brown@example.com", "50", "Consultant", "Florida", "10000"),
+            new TreeTableViewWithSubheaders.Employee("James Brown", "anna.black@example.com", "45", "Consultant", "Boston", "20000"),
+            new TreeTableViewWithSubheaders.Employee("Linda Gates", "roger.york@example.com", "56", "Manager", "Washington", "5000"));
+
+    final TreeItem<TreeTableViewWithSubheaders.Employee> root =
+            new TreeItem<>(new TreeTableViewWithSubheaders.Employee("Employees", "", "", "", "", ""));
+    final TreeItem<TreeTableViewWithSubheaders.Employee> salesRoot =
+            new TreeItem<>(new TreeTableViewWithSubheaders.Employee("Sales Department", "", "", "", "", ""));
+    final TreeItem<TreeTableViewWithSubheaders.Employee> itRoot =
+            new TreeItem<>(new TreeTableViewWithSubheaders.Employee("IT Department", "", "", "", "", ""));
+
+    public static void main(String[] args) {
+        Application.launch(TreeTableViewWithSubheaders.class, args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Style startingStyle = Style.LIGHT;
+        JMetro jMetro = new JMetro(startingStyle);
+
+        System.setProperty("prism.lcdtext", "false");
+
+        root.getChildren().add(salesRoot);
+        root.getChildren().add(itRoot);
+
+        employees.forEach((employee) -> salesRoot.getChildren().add(new TreeItem<>(employee)));
+        itEmployees.forEach((employee) -> itRoot.getChildren().add(new TreeItem<>(employee)));
+
+        stage.setTitle("Tree Table View With Subheaders Sample");
+        final Scene scene = new Scene(new BorderPane());
+        BorderPane sceneRoot = (BorderPane) scene.getRoot();
+
+        TreeTableColumn<TreeTableViewWithSubheaders.Employee, String> empColumn = new TreeTableColumn<>("Employee");
+        empColumn.setPrefWidth(170);
+        empColumn.setCellValueFactory(
+                (TreeTableColumn.CellDataFeatures<TreeTableViewWithSubheaders.Employee, String> param) ->
+                        new ReadOnlyStringWrapper(param.getValue().getValue().getName())
+        );
+
+        TreeTableColumn<TreeTableViewWithSubheaders.Employee, String> emailColumn = new TreeTableColumn<>("Email");
+        emailColumn.setPrefWidth(220);
+        emailColumn.setCellValueFactory(
+                (TreeTableColumn.CellDataFeatures<TreeTableViewWithSubheaders.Employee, String> param) ->
+                        new ReadOnlyStringWrapper(param.getValue().getValue().getEmail())
+        );
+
+        TreeTableColumn<TreeTableViewWithSubheaders.Employee, String> ageColumn = new TreeTableColumn<>("Age");
+        ageColumn.setPrefWidth(60);
+        ageColumn.setCellValueFactory(
+                (TreeTableColumn.CellDataFeatures<TreeTableViewWithSubheaders.Employee, String> param) ->
+                        new ReadOnlyStringWrapper(param.getValue().getValue().getAge())
+        );
+
+        TreeTableColumn<TreeTableViewWithSubheaders.Employee, String> positionColumn = new TreeTableColumn<>("Position");
+        positionColumn.setPrefWidth(130);
+        positionColumn.setCellValueFactory(
+                (TreeTableColumn.CellDataFeatures<TreeTableViewWithSubheaders.Employee, String> param) ->
+                        new ReadOnlyStringWrapper(param.getValue().getValue().getPosition())
+        );
+
+
+        TreeTableColumn<TreeTableViewWithSubheaders.Employee, String> cityColumn = new TreeTableColumn<>("City");
+        cityColumn.setPrefWidth(130);
+        cityColumn.setCellValueFactory(
+                (TreeTableColumn.CellDataFeatures<TreeTableViewWithSubheaders.Employee, String> param) ->
+                        new ReadOnlyStringWrapper(param.getValue().getValue().getCity())
+        );
+
+        TreeTableColumn<TreeTableViewWithSubheaders.Employee, String> postalCodeColumn = new TreeTableColumn<>("Postal Code");
+        postalCodeColumn.setPrefWidth(130);
+        postalCodeColumn.setCellValueFactory(
+                (TreeTableColumn.CellDataFeatures<TreeTableViewWithSubheaders.Employee, String> param) ->
+                        new ReadOnlyStringWrapper(param.getValue().getValue().getPostalCode())
+        );
+
+        TreeTableColumn<TreeTableViewWithSubheaders.Employee, String> addressColumn = new TreeTableColumn<>("Address");
+        addressColumn.getColumns().addAll(cityColumn, postalCodeColumn);
+
+        TreeTableView<TreeTableViewWithSubheaders.Employee> treeTableView = new TreeTableView<>(root);
+        treeTableView.getColumns().setAll(empColumn, emailColumn, ageColumn, positionColumn, addressColumn);
+
+        salesRoot.setExpanded(true);
+        treeTableView.setShowRoot(false);
+
+        sceneRoot.setCenter(treeTableView);
+
+        // Customization controls
+        HBox controlsHBox = new HBox();
+        controlsHBox.setSpacing(10);
+
+        CheckBox cellSelectionCheckBox = new CheckBox("Cell Sbelection");
+        cellSelectionCheckBox.setOnAction(event -> {
+            treeTableView.getSelectionModel().setCellSelectionEnabled(cellSelectionCheckBox.isSelected());
+        });
+        cellSelectionCheckBox.setSelected(treeTableView.getSelectionModel().isCellSelectionEnabled());
+
+        CheckBox tableButtonCheckBox = new CheckBox("Table Menu Button");
+        tableButtonCheckBox.setOnAction(event -> {
+            treeTableView.setTableMenuButtonVisible(tableButtonCheckBox.isSelected());
+        });
+        tableButtonCheckBox.setSelected(treeTableView.isTableMenuButtonVisible());
+
+        CheckBox alternatingRowColors = new CheckBox("Alternating Row Colors");
+        alternatingRowColors.setOnAction(event -> {
+            String alternatingRowColorsStyleClass = "alternating-row-colors";
+            boolean isSelected = alternatingRowColors.isSelected();
+            if (isSelected) {
+                if (!treeTableView.getStyleClass().contains(alternatingRowColorsStyleClass)) {
+                    treeTableView.getStyleClass().add(alternatingRowColorsStyleClass);
+                }
+            } else {
+                treeTableView.getStyleClass().remove(alternatingRowColorsStyleClass);
+            }
+        });
+
+        CheckBox columnGridLines = new CheckBox("Grid Lines");
+        columnGridLines.setOnAction(event -> {
+            boolean isSelected = columnGridLines.isSelected();
+            if (isSelected) {
+                addIfNotPresent(treeTableView.getStyleClass(), TABLE_GRID_LINES);
+            } else {
+                treeTableView.getStyleClass().remove(TABLE_GRID_LINES);
+            }
+        });
+
+        ComboBox<Style> jmetroStyleComboBox = new ComboBox<>();
+        jmetroStyleComboBox.getItems().addAll(Style.DARK, Style.LIGHT);
+        jmetroStyleComboBox.setValue(startingStyle);
+        jmetroStyleComboBox.valueProperty().addListener(observable -> jMetro.setStyle(jmetroStyleComboBox.getValue()));
+
+        controlsHBox.getChildren().addAll(jmetroStyleComboBox, cellSelectionCheckBox, tableButtonCheckBox, alternatingRowColors, columnGridLines);
+        sceneRoot.setBottom(controlsHBox);
+
+        controlsHBox.getStyleClass().add("background");
+
+        stage.setScene(scene);
+
+        jMetro.setScene(scene);
+
+        stage.show();
+    }
+
+    public class Employee {
+
+        private SimpleStringProperty name;
+        private SimpleStringProperty email;
+        private SimpleStringProperty age;
+        private SimpleStringProperty position;
+        private SimpleStringProperty city;
+        private StringProperty postalCode;
+
+        private Employee(String name, String email, String age, String position, String city, String postalCode) {
+            this.name = new SimpleStringProperty(name);
+            this.email = new SimpleStringProperty(email);
+            this.age = new SimpleStringProperty(age);
+            this.position = new SimpleStringProperty(position);
+            this.city = new SimpleStringProperty(city);
+            this.postalCode = new SimpleStringProperty(postalCode);
+        }
+
+        public String getName() { return name.get(); }
+        public void setName(String fName) { name.set(fName); }
+        public SimpleStringProperty nameProperty() {
+            if (name == null) {
+                name = new SimpleStringProperty(this, "name");
+            }
+            return name;
+        }
+
+        public String getEmail() { return email.get(); }
+        public void setEmail(String fName) { email.set(fName); }
+        public SimpleStringProperty emailProperty() {
+            if (email == null) {
+                email = new SimpleStringProperty(this, "email");
+            }
+            return email;
+        }
+
+        public String getAge() { return age.get(); }
+        public void setAge(String age) { this.age.set(age); }
+        public SimpleStringProperty ageProperty() {
+            return age;
+        }
+
+        public String getPosition() { return position.get(); }
+        public void setPosition(String position) { this.position.set(position); }
+        public SimpleStringProperty positionProperty() {
+            return position;
+        }
+
+        public String getCity() { return city.get(); }
+        public void setCity(String city) { this.city.set(city); }
+        public SimpleStringProperty cityProperty() {
+            return city;
+        }
+
+        public String getPostalCode() { return postalCode.get(); }
+        public void setPostalCode(String postalCode) { this.postalCode.set(postalCode); }
+        public StringProperty postalCodeProperty() { return postalCode; }
+    }
+}
